3d49d1794e60630194667518d85a0cac31d2bdc4
Mohammed Rimshan <rimshanshanu55@gmail.com>
2025-07-13 22:25:24 +0530

    chore: removed unwanted comments from hooks

--- Client/src/hooks/me/useUser.ts
+++ Client/src/hooks/me/useUser.ts
@@ -5,6 +5,6 @@ export const useGetMe = () => {
   return useQuery({
     queryKey: ['me'],
     queryFn: getMe,
-    staleTime: 10 * 60 * 1000, // 10 minutes
+    staleTime: 10 * 60 * 1000, 
   });
 };
--- Client/src/hooks/url/useUrl.ts
+++ Client/src/hooks/url/useUrl.ts
@@ -5,7 +5,6 @@ export const useCreateUrl = () => {
   return useMutation({
     mutationFn: createUrl,
     onSuccess: (_data, _variables, _context) => {
-      // Invalidate URLs query to refresh the list
       const queryClient = new QueryClient();
       queryClient.invalidateQueries({ queryKey: ['urls'] });
     },
@@ -16,7 +15,7 @@ export const useGetUrls = () => {
   return useQuery({
     queryKey: ['urls'],
     queryFn: getUrls,
-    staleTime: 5 * 60 * 1000, // 5 minutes
+    staleTime: 5 * 60 * 1000, 
   });
 };
 
@@ -24,7 +23,7 @@ export const useGetAnalytics = (urlId: string) => {
   return useQuery({
     queryKey: ['analytics', urlId],
     queryFn: () => getAnalytics(urlId),
-    enabled: !!urlId, // Only run if urlId is provided
-    staleTime: 5 * 60 * 1000, // 5 minutes
+    enabled: !!urlId, 
+    staleTime: 5 * 60 * 1000, 
   });
 };
